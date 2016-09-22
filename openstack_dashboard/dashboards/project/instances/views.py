d10225804990711b924b5df0e271b184afc44da7
Corey Bryant <corey.bryant@canonical.com>
2016-09-22 18:20:21 -0500

    test

--- openstack_dashboard/dashboards/project/instances/views.py
+++ openstack_dashboard/dashboards/project/instances/views.py
@@ -58,7 +58,7 @@ LOG = logging.getLogger(__name__)
 class IndexView(tables.DataTableView):
     table_class = project_tables.InstancesTable
     template_name = 'project/instances/index.html'
-    page_title = _("Instances")
+    page_title = _("Instances ARE AWESOME!!!!")
 
     def has_more_data(self, table):
         return self._more
